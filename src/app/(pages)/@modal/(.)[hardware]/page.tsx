22f0fb272f6bf86cd86d4516344d10870f249c99
duyanhdinh <duyanhdinh@outlook.com>
2023-10-05 13:17:08 +0700

    use dynamic route

---
+++ src/app/(pages)/@modal/(.)[hardware]/page.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import Motherboard from "@components/pages/motherboard/motherboard";
+import React, {useEffect, useState} from "react";
+import ModalAutoShow from "@components/common/modal-auto-show";
+import Ram from "@components/pages/ram/ram";
+import Monitor from "@components/pages/monitor/monitor";
+import {RELEASE_URI_HARDWARE} from "@/app/(pages)/[hardware]/page";
+const getComponent = (hardware: string): React.ReactNode => {
+    switch (hardware) {
+        case RELEASE_URI_HARDWARE.MOTHERBOARD:
+            return (<Motherboard/>);
+        case RELEASE_URI_HARDWARE.RAM:
+            return (<Ram/>);
+        case RELEASE_URI_HARDWARE.MONITOR:
+            return (<Monitor/>);
+        default:
+            return null;
+    }
+};
+
+const modalId = 'modal_hardware';
+
+/***
+ Not working: https://github.com/vercel/next.js/issues/52533
+ */
+export default function Page ({params : { hardware }}: { params: { hardware: string } }) {
+    const [content, setContent] = useState<React.ReactNode>(null);
+
+    useEffect(() => {
+        setContent(getComponent(hardware));
+    }, [hardware]);
+
+    return (
+        <ModalAutoShow content={content} modalId={modalId} />
+    );
+}
---
+++ src/app/(pages)/[hardware]/page.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import React, {useEffect, useState} from "react";
+import Monitor from "@components/pages/monitor/monitor";
+import {useAppDispatch} from "@store/hooks";
+import {getLocalMotherboard} from "@store/features/hardware/motherboard/motherboardSlice";
+import {getLocalRam} from "@store/features/hardware/ram/ramSlice";
+import {getLocalMonitor} from "@store/features/hardware/monitor/monitorSlice";
+import Motherboard from "@components/pages/motherboard/motherboard";
+import Ram from "@components/pages/ram/ram";
+
+const RELEASE_URI_HARDWARE = {
+    MOTHERBOARD: 'motherboard',
+    RAM: 'ram',
+    MONITOR: 'monitor'
+};
+
+const getComponent = (hardware: string): React.ReactNode => {
+    switch (hardware) {
+        case RELEASE_URI_HARDWARE.MOTHERBOARD:
+            return (<Motherboard/>);
+        case RELEASE_URI_HARDWARE.RAM:
+            return (<Ram/>);
+        case RELEASE_URI_HARDWARE.MONITOR:
+            return (<Monitor/>);
+        default:
+            return null;
+    }
+};
+
+const getLocalValue = (dispatch: ReturnType<typeof useAppDispatch>, hardware: string): void => {
+    switch (hardware) {
+        case RELEASE_URI_HARDWARE.MOTHERBOARD:
+            dispatch(getLocalMotherboard());
+            break;
+        case RELEASE_URI_HARDWARE.RAM:
+            dispatch(getLocalRam());
+            break;
+        case RELEASE_URI_HARDWARE.MONITOR:
+            dispatch(getLocalMonitor());
+            break;
+    }
+};
+
+export default function Page({params : { hardware }}: { params: { hardware: string } }) {
+    const dispatch = useAppDispatch();
+    const [Hardware, setHardware] = useState<React.ReactNode>(null);
+
+    useEffect(() => {
+        getLocalValue(dispatch, hardware);
+
+        setHardware(getComponent(hardware));
+    }, [dispatch, hardware]);
+
+    return (
+        <>{Hardware}</>
+    );
+}
+
+export { RELEASE_URI_HARDWARE };
